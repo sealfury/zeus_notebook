0527e72b20fbd1723025f044e2a114d180727003
William <willspecker@gmail.com>
2021-03-31 23:29:54 +0200

    begins refactor of plugin to dynamically fetch modules from unpkg imports message with commonjs for consistency

--- src/plugins/unpkg_path_plugin.ts
+++ src/plugins/unpkg_path_plugin.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import * as esbuild from 'esbuild-wasm'
 
 export const unpkgPathPlugin = () => {
@@ -6,7 +7,14 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args)
-        return { path: args.path, namespace: 'a' }
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' }
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a',
+          }
+        }
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,11 +24,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-          import message from 'tiny-test-pkg'
+          const message = require('tiny-test-pkg')
           console.log(message)
           `,
           }
-        } 
+        }
+
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data,
+        }
       })
     },
   }
