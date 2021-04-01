e39eb4574292457c3f207b83133daae235140d96
William <willspecker@gmail.com>
2021-04-01 15:06:31 +0200

    extracts onResolve conditionals into separate functions

--- src/plugins/unpkg_path_plugin.ts
+++ src/plugins/unpkg_path_plugin.ts
@@ -10,22 +10,24 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // handle root entry file of index.js
+      build.onResolve({ filter: /(^index\.js$)/}, () => {
+        return { path: 'index.js', namespace: 'a' }
+      })
+
+      // handle relative paths
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href,
+        }
+      })
+
+      // handle module main file
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args)
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' }
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          }
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
