ceea48d2bd571856725b3fd79f8e58c36a0ea01d
William <willspecker@gmail.com>
2021-04-01 17:51:13 +0200

    creats iframe to hold bundled code via srcDoc

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,11 @@ const App = () => {
     setCode(result.outputFiles[0].text)
   }
 
+  const html = `
+    <script>
+      ${code}
+    </script>
+  `
   return (
     <div>
       <textarea
@@ -48,6 +53,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox='allow-scripts' srcDoc={html}></iframe>
     </div>
   )
 }
