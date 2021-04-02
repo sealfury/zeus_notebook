e5fd7b5d42ec9f6c37878115437e3d7f6a7ae556
William <willspecker@gmail.com>
2021-04-02 17:49:45 +0200

    implements code formatting in editor via prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react'
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
 
 interface CodeEditorProps {
   initialValue: string
@@ -6,7 +9,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>()
+
   const onEditorDidMount: EditorDidMount = (getCurrentValue, monacoEditor) => {
+    editorRef.current = monacoEditor
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getCurrentValue())
     })
@@ -14,24 +20,44 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
   }
 
+  const onFormatClick = () => {
+    // get current val from editor
+    const unformatted = editorRef.current.getModel().getValue()
+
+    // format value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: false, // maybe change this later
+      singleQuote: true,
+    })
+
+    // set formatted val in editor
+    editorRef.current.setValue(formatted)
+  }
+
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      value={initialValue}
-      theme='vs-dark'
-      language='javascript'
-      height='500px'
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        theme='vs-dark'
+        language='javascript'
+        height='500px'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
   )
 }
 
