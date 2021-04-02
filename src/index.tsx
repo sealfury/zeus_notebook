a990ea4c5221ecb9b7d3a3173dc91d4b41e12ce8
William <willspecker@gmail.com>
2021-04-02 22:28:26 +0200

    refactors preview into new component

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg_path_plugin'
 import { fetchPlugin } from './plugins/fetch_plugin'
 import CodeEditor from './components/code-editor'
+import Preview from './components/preview'
 
 const App = () => {
   const ref = useRef<any>()
-  const iframe = useRef<any>()
+  const [code, setCode] = useState('')
   const [input, setInput] = useState('')
 
   const startService = async () => {
@@ -26,8 +27,6 @@ const App = () => {
       return
     }
 
-    iframe.current.srcdoc = html
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -39,45 +38,19 @@ const App = () => {
       },
     })
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    setCode(result.outputFiles[0].text)
   }
 
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data)
-            } catch (err) {
-              const root = document.querySelector('#root')
-              root.innerHTML = 
-                '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-              console.error(err)
-            }
-          }, false)
-        </script>
-      </body>
-    </html>
-  `
   return (
     <div>
-      <CodeEditor initialValue='const x = 1' onChange={(value) => setInput(value)}/>
-      <textarea
-        value={input}
-        onChange={e => setInput(e.target.value)}
-      ></textarea>
+      <CodeEditor
+        initialValue='const x = 1'
+        onChange={value => setInput(value)}
+      />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title='preview'
-        ref={iframe}
-        sandbox='allow-scripts'
-        srcDoc={html}
-      ></iframe>
+      <Preview code={code} />
     </div>
   )
 }
