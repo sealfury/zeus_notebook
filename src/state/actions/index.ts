dafc7fc21881403af373ab05ac70275da602be25
William <willspecker@gmail.com>
2021-04-03 20:12:44 +0200

    connects redux & defines and implements update action creator

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,32 +1,34 @@
 import { ActionType } from '../action-types'
 import { CellTypes } from '../cell'
 
-interface MoveCellAction {
-  type: ActionType.MOVE_CELL
-  payload: {
-    id: string
-    direction: 'up' | 'down'
-  }
-}
+export type Direction = 'up' | 'down'
 
-interface DeleteCellAction {
-  type: ActionType.DELETE_CELL
-  payload: string
-}
-
-interface InsertCellBeforeAction {
-  type: ActionType.INSERT_CELL_BEFORE
-  payload: {
-    id: string
-    type: CellTypes
-  }
-}
-
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL
   payload: {
     id: string
     content: string
+  }
+}
+
+export interface DeleteCellAction {
+  type: ActionType.DELETE_CELL
+  payload: string
+}
+
+export interface MoveCellAction {
+  type: ActionType.MOVE_CELL
+  payload: {
+    id: string
+    direction: Direction
+  }
+}
+
+export interface InsertCellBeforeAction {
+  type: ActionType.INSERT_CELL_BEFORE
+  payload: {
+    id: string
+    type: CellTypes
   }
 }
 
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -23,13 +23,23 @@ const reducer = (
   action: Action
 ): CellState => {
   switch (action.type) {
-    case ActionType.MOVE_CELL:
-      return state
+    case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [id]: {
+            ...state.data[id],
+            content,
+          },
+        },
+      }
     case ActionType.DELETE_CELL:
       return state
-    case ActionType.INSERT_CELL_BEFORE:
+    case ActionType.MOVE_CELL:
       return state
-    case ActionType.UPDATE_CELL:
+    case ActionType.INSERT_CELL_BEFORE:
       return state
     default:
       return state
