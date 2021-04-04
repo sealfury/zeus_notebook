c221d5a9e44d586fa2ceec2781d242742ff4f231
William <willspecker@gmail.com>
2021-04-04 21:58:18 +0200

    adds bundling action types

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -3,4 +3,6 @@ export enum ActionType {
   DELETE_CELL = 'delete_cell',
   MOVE_CELL = 'move_cell',
   INSERT_CELL_AFTER = 'insert_cell_after',
+  BUNDLE_START = 'bundle_start',
+  BUNDLE_COMPLETE = 'bundle_complete'
 }
