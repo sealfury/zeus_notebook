6f420b0aab89e8ff7e72c47f9876568000c238f4
William <willspecker@gmail.com>
2021-04-04 22:34:06 +0200

    implements bundling action creators and adjusts reducer state to account for undefined value of bundle state upon app load

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,42 +1,45 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import CodeEditor from './code-editor'
 import Preview from './preview'
-import bundleCode from '../bundler'
 import Resizable from './resizable'
 import { Cell } from '../state'
 import { useActions } from '../hooks/use-actions'
+import { useTypedSelector } from '../hooks/use-typed-selector'
 
 interface CodeCellProps {
   cell: Cell
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('')
-  const [err, setErr] = useState('')
-  const { updateCell } = useActions()
+  const { updateCell, createBundle } = useActions()
+  const bundle = useTypedSelector(state => state.bundles[cell.id])
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundleCode(cell.content)
-      setCode(output.code)
-      setErr(output.err)
+      createBundle(cell.id, cell.content)
     }, 1000)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [cell.content])
+  }, [cell.id, cell.content])
 
   return (
     <Resizable direction='vertical'>
-      <div style={{ height: 'calc(100% - 8px)', display: 'flex', flexDirection: 'row' }}>
+      <div
+        style={{
+          height: 'calc(100% - 8px)',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
         <Resizable direction='horizontal'>
           <CodeEditor
             initialValue={cell.content}
             onChange={value => updateCell(cell.id, value)}
           />
         </Resizable>
-        <Preview code={code} error={err} />
+        {bundle && <Preview code={bundle.code} error={bundle.err} />}
       </div>
     </Resizable>
   )
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from 'redux'
 import { ActionType } from '../action-types'
 import {
   Action,
@@ -5,9 +6,12 @@ import {
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
+  BundleStartAction,
+  BundleCompleteAction,
   Direction,
 } from '../actions'
 import { CellTypes } from '../cell'
+import bundleCode from '../../bundler'
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -46,5 +50,26 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  }
+}
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    })
+
+    const result = await bundleCode(input)
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      },
+    })
   }
 }
