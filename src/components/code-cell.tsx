a28f4d3ef026d52140cddce549193b58048bc953
William <willspecker@gmail.com>
2021-04-04 15:54:03 +0200

    extracts input state in code cell component to useActions hook extracts value state in text cell component to useActions hook

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,31 +3,37 @@ import CodeEditor from './code-editor'
 import Preview from './preview'
 import bundleCode from '../bundler'
 import Resizable from './resizable'
+import { Cell } from '../state'
+import { useActions } from '../hooks/use-actions'
 
-const CodeCell = () => {
+interface CodeCellProps {
+  cell: Cell
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('')
   const [err, setErr] = useState('')
-  const [input, setInput] = useState('')
+  const { updateCell } = useActions()
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundleCode(input)
+      const output = await bundleCode(cell.content)
       setCode(output.code)
       setErr(output.err)
-    }, 1000) 
+    }, 1000)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [input])
+  }, [cell.content])
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
           <CodeEditor
-            initialValue='const x = 1'
-            onChange={value => setInput(value)}
+            initialValue={cell.content}
+            onChange={value => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} error={err} />
