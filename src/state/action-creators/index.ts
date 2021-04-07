d40c1ea106c592c2553ced9ccdc79111c8681322
William <willspecker@gmail.com>
2021-04-07 21:07:19 +0200

    adds cell fetching logic to action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from 'redux'
+import axios from 'axios'
 import { ActionType } from '../action-types'
 import {
   Action,
@@ -8,7 +9,7 @@ import {
   InsertCellAfterAction,
   Direction,
 } from '../actions'
-import { CellTypes } from '../cell'
+import { Cell, CellTypes } from '../cell'
 import bundleCode from '../../bundler'
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -69,5 +70,25 @@ export const createBundle = (cellId: string, input: string) => {
         bundle: result,
       },
     })
+  }
+}
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.FETCH_CELLS })
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get('/cells')
+
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      })
+    } catch (err) {
+      dispatch({
+        type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      })
+    }
   }
 }
